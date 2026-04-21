6c5827855aa7d3e1281c817a797f1b87ccaf4184
Joshué Agapé <agapedev.dark@gmail.com>
2026-04-21 18:25:23 +0300

    feat(database): add full database configuration, models, migrations, and env setup

---
+++ index.ps1
@@ -0,0 +1,5 @@
+# . "$HOME\.config\alias\fastapi-project\create-fastapi-project.ps1"
+# . "$HOME\.config\alias\fastapi-project\sqlalchemy.ps1"
+
+. "C:\Users\DARK\Desktop\MyProjects\ALIAS\fastapi-project\create-fastapi-project.ps1"
+. "C:\Users\DARK\Desktop\MyProjects\ALIAS\fastapi-project\sqlalchemy.ps1"
